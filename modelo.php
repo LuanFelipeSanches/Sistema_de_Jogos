00c746e9c6342d1e93e0f813bb8fad75820f30de
LuanFelipeSanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-05-23 23:22:08 -0400

    Index,modelo e css

---
+++ modelo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+       <title>Título da Página</title>
+       <meta charset="utf-8"/>
+       <link rel="stylesheet" href="estilos/estilo.css"/>
+</head>
+<body>
+    <div id="corpo">
+<h1>Olá</h1>
+    </div>
+</body>
+</html>
