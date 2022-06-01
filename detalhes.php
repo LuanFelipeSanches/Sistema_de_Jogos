bc999275fdae05cf90220290e2ee26b2aa898486
LuanFelipeSanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-05-31 23:59:41 -0400

    Criando pagina de detalhes

--- detalhes.php
+++ detalhes.php
@@ -13,30 +13,33 @@
     require_once "includes/funcoes.php";
     ?>
     <div id="corpo">
-        <?php 
+        <?php
         $c = $_GET['cod'] ?? 0;
         $busca = $banco->query("SELECT * FROM jogos WHERE cod ='$c'");
-      
+
         ?>
         <h1>Detalhes do jogo</h1>
         <table class='detalhes'>
-<?php
-if(!$busca){
-    echo "<tr><td>Busca falhou! $banco->error";
-}else{
-    if($busca->num_rows ==1){
-        $reg = $busca->fetch_object();
-        echo "<tr><td rowspan='3'>foto";
-        echo "<td> <h2>$reg->nome</h2>";
-        echo "<td>Descrição";
-        echo "<tr><td>Adm";
-    }else{
-        echo "<tr><td>Nenhum registro encontrado</td></tr>";
-    }
-}
-?>        </table>
+            <?php
+            if (!$busca) {
+                echo "<tr><td>Busca falhou! $banco->error";
+            } else {
+                if ($busca->num_rows == 1) {
+                    $reg = $busca->fetch_object();
+                    $t = thumb($reg->capa);
+                    echo "<tr><td rowspan='3'><img src='$t' class='full'/>";
+                    echo "<td><h2>$reg->nome</h2>";
+                    echo "Nota:" . number_format($reg->nota, 1) . "/10.0";
+                    echo "<tr><td>$reg->descricao";
+                    echo "<tr><td>Adm";
+                } else {
+                    echo "<tr><td>Nenhum registro encontrado";
+                }
+            }
+            ?> </table>
+        <a href="index.php"><img src="icones/icoback.png">
+        </a>
     </div>
 </body>
 
 </html>
-
